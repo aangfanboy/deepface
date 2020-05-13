a6c111c03c76a2d86ca799943ba016512efe6e8c
aangfanboy <buraktoy161@gmail.com>
2020-05-13 10:20:54 +0300

    aligner for MTCNN added

--- face_detection/mtcnn_detector.py
+++ face_detection/mtcnn_detector.py
@@ -1,7 +1,10 @@
 import cv2
+import math
+import numpy as np
 
-from face_detection.detector_main import MainHelper
+from PIL import Image
 from mtcnn.mtcnn import MTCNN
+from face_detection.detector_main import MainHelper
 
 
 class Engine(MainHelper):
@@ -63,6 +66,49 @@ class Engine(MainHelper):
 
 		return frames		
 
+	@staticmethod
+	def euclidean_distance(a, b):
+		x1 = a[0]
+		y1 = a[1]
+
+		x2 = b[0]
+		y2 = b[1]
+		return math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)))
+
+	def align_image_from_eyes(self, image, eyes):
+		left_eye, right_eye = eyes[0]
+		left_x, left_y = left_eye
+		right_x, right_y = right_eye
+
+		x_diff = float(abs(right_x - left_x))
+		y_diff = float(abs(right_y - left_y))
+		c_dist = math.sqrt(x_diff**2 + y_diff**2)
+
+		alpha_ = np.arccos((c_dist**2 + x_diff**2 - y_diff ** 2)/(2*x_diff*c_dist))
+		alpha = (alpha_ * 180) / math.pi
+
+		if left_y < right_y:
+			direction = 1
+		else:
+			direction = -1
+			# alpha = 90 - alpha
+
+		new_img = Image.fromarray(image)
+		new_img = np.array(new_img.rotate(direction * alpha))
+		return new_img
+
+	def get_boxes_from_faces_with_eyes(self, faces, th: float = None):
+		boxes = []
+		eyes = []
+		for face in faces:
+			if th is not None and face["confidence"] < th:
+				continue
+
+			boxes.append(face["box"])
+			eyes.append((face["keypoints"]["mouth_left"], face["keypoints"]["mouth_right"]))
+
+		return boxes, eyes
+
 	def get_boxes_from_faces(self, faces, th: float = None):
 		boxes = []
 		for face in faces:
@@ -79,7 +125,10 @@ if __name__ == '__main__':
 
 	image = e.load_image("test.jpg")
 	faces = e.get_faces_from_image(image)
-	boxes = e.get_boxes_from_faces(faces)
+	boxes, eyes = e.get_boxes_from_faces_with_eyes(faces)
+	image = e.align_image_from_eyes(image, eyes)
+	faces = e.get_faces_from_image(image)
+	boxes, eyes = e.get_boxes_from_faces_with_eyes(faces)
 	image = e.draw_faces_on_image(image, boxes, color="different")
 
 	e.display_image(image, destroy_after=False, n=0)
